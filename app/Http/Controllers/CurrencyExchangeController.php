9cc7fffebe631fe918b7381c733fb48a007ba867
Ryan <ryan_chin@mail.chungyo.net>
2023-10-21 16:35:44 +0800

    修正 API 回傳中文時亂碼的問題

--- app/Http/Controllers/CurrencyExchangeController.php
+++ app/Http/Controllers/CurrencyExchangeController.php
@@ -19,7 +19,7 @@ class CurrencyExchangeController extends Controller
 
         if ($validator->fails()) {
             $errorContent = ['msg' => '缺少欄位'];
-            return response()->json($errorContent, Response::HTTP_BAD_REQUEST);
+            return response()->json($errorContent, Response::HTTP_BAD_REQUEST, [], JSON_UNESCAPED_UNICODE);
         }
 
         try {
@@ -34,11 +34,11 @@ class CurrencyExchangeController extends Controller
             $result = $service->execute($source, $target, $amount);
         } catch (InvalidArgumentException $e) {
             $errorContent = ['msg' => $e->getMessage()];
-            return response()->json($errorContent, Response::HTTP_BAD_REQUEST);
+            return response()->json($errorContent, Response::HTTP_BAD_REQUEST, [], JSON_UNESCAPED_UNICODE);
         } catch (\Throwable $th) {
             $errorContent = ['msg' => $th->getMessage()];
-            return response()->json($errorContent, Response::HTTP_INTERNAL_SERVER_ERROR);
+            return response()->json($errorContent, Response::HTTP_INTERNAL_SERVER_ERROR, [], JSON_UNESCAPED_UNICODE);
         }
-        return response()->json(['msg' => 'success', 'amount' => $result], Response::HTTP_OK);
+        return response()->json(['msg' => 'success', 'amount' => $result], Response::HTTP_OK, [], JSON_UNESCAPED_UNICODE);
     }
 }
